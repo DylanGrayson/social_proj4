e92ae1d622b749a7bb364c222afe02eb50b9494f
Michael Gilbert <drumz4jah@yahoo.com>
2015-02-28 00:08:30 -0700

    added success message to registration
    also added a couple more test users

--- main/views/register.py
+++ main/views/register.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib import messages
 
 def new_user(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            messages.success(request, 'You have been registered')
             new_user = form.save()
             return HttpResponseRedirect("/register/")
     elif request.method == 'GET':
