88925f947bbce3d367a85bebd3837518a7fd6840
Michael Gilbert <drumz4jah@yahoo.com>
2015-03-02 23:57:34 -0700

    Created edit user html template and added view to urls

--- main/urls.py
+++ main/urls.py
@@ -8,6 +8,7 @@ urlpatterns = patterns('',
                        url(r'^logout/', 'main.views.login.user_logout', name='user_logout'),
                        url(r'^user/(?P<num>\d+)/$', 'main.views.main.profile', name='user'),
                        url(r'^search/', 'main.views.main.search_results', name='search_results'),
+                       url(r'^user/edit/(?P<num>\d+)/$', 'main.views.edit_user.edit', name='edituser'),
                        url(r'^admin/', include(admin.site.urls)),
 
                        )
