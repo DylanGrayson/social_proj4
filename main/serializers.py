f04033952e01c91fd07f253de05af591c59c0610
Michael Gilbert <drumz4jah@yahoo.com>
2015-03-30 23:19:03 -0700

    Made Recent Posts update every 3 secs on focus...
    Also added links to the user of each post

--- main/serializers.py
+++ main/serializers.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 class UserSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'first_name', 'last_name')
+		fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
 
 class PostSerializer(serializers.ModelSerializer):
